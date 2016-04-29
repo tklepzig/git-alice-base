9417adbd98bd7a2e6c205dc3aa87c1754c9b158f
Thomas <Thomas@DevBook>
2016-04-29 13:50:08 +0200

    resetting back to first commit instead of current master

--- reset.sh
+++ reset.sh
@@ -5,6 +5,6 @@ do
 	#delete develop branch
 	git push alice-$i :develop
 
-	#revert master back to base-master
-	git push -f alice-$i master
+	#revert master back to init
+	git push -f alice-$i init:master
 done
