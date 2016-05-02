ac6ddcde155cd2dd9961e7986eb4989a81f0fd69
Thomas <Thomas@DevBook>
2016-05-02 09:29:50 +0200

    delete tags when doing a reset

--- reset.sh
+++ reset.sh
@@ -5,6 +5,9 @@ do
 	#delete develop branch
 	git push alice-$i :develop
 
+	#delete tags
+	git push alice-$i :refs/tags/v1.0.0
+
 	#revert master back to init
 	git push -f alice-$i init:master
 done
